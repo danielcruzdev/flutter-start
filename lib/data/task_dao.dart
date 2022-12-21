d74308f6495c472beecce9158d00002eab9d601f
Daniel Cruz <d.afc98@gmail.com>
2022-12-20 22:32:44 -0300

    criando crud com sqflite

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
+import 'package:sqflite/sqlite_api.dart';
 import '../components/task.dart';
+import 'database.dart';
 
 class TaskDao {
   static const String _tableName = 'task';
@@ -11,11 +13,75 @@ class TaskDao {
       '$_difficulty INTEGER,'
       '$_image TEXT)';
 
-  Future<List<Task>> findAll() async {}
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll: ');
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+    print('Procuranco dados no banco de dados... encontrado $result');
+    return toList(result);
+  }
 
-  Future<Task> find(String nomeTarefa) async {}
+  Future<List<Task>> find(String nomeTarefa) async {
+    print('Acessando find: ');
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db
+        .query(_tableName, where: '$_name = ?', whereArgs: [nomeTarefa]);
 
-  save(Task task) async {}
+    print('tarefa encontrada: ${toList(result)}');
+    return toList(result);
+  }
 
-  delete(String nomeTarefa) async {}
+  save(Task task) async {
+    print('iniciando o save: ');
+    final db = await getDatabase();
+    var itemExists = await find(task.nome);
+    Map<String, dynamic> taskMap = toMap(task);
+    if (itemExists.isEmpty) {
+      print('A tarefa não existe.');
+      return await db.insert(_tableName, taskMap);
+    } else {
+      print('A tarefa ja existe');
+      return await db.update(
+        _tableName,
+        taskMap,
+        where: '$_name = ?',
+        whereArgs: [task.nome],
+      );
+    }
+  }
+
+  delete(String nomeTarefa) async {
+    print('deletando tarefa: ');
+    final db = await getDatabase();
+    return db.delete(
+      _tableName,
+      where: '$_name = ?',
+      whereArgs: [nomeTarefa],
+    );
+  }
+
+  Map<String, dynamic> toMap(Task tarefa) {
+    print('Convertendo tarefa em Map');
+    final Map<String, dynamic> mapaDeTarefas = Map();
+    mapaDeTarefas[_name] = tarefa.nome;
+    mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+    mapaDeTarefas[_image] = tarefa.foto;
+
+    print('mapa de tarefa: $mapaDeTarefas');
+
+    return mapaDeTarefas;
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('Convertendo map para list');
+    final List<Task> tarefas = [];
+
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+
+    print('Lista de tarefas: $tarefas');
+    return tarefas;
+  }
 }
